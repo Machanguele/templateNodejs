d47e30e56fe86a86d92b08ad1fc15a92c46e0e12
Jose Machanguele <josemachanguele@gmail.com>
2022-05-15 04:24:14 +0200

    ADD: Unit Tests

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import {UsersController} from "./Controllers/UsersController";
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (request, response) => {
     response.json({hello: 'Im configuring Node Type Script'})
